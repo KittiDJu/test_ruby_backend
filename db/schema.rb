826979bf7d2044543d9f06011fe3b8b879fd3d4c
KittiDJu <kittiponjunlapak@gmail.com>
2023-09-19 23:09:53 +0700

    v1.0

---
+++ db/schema.rb
@@ -0,0 +1,19 @@
+ActiveRecord::Schema[7.0].define(version: 2023_09_19_040356) do
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
+
+  add_foreign_key "posts", "authors"
+end
